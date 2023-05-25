80f53951653ab12e4f4be52918478845f2e0f642
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2023-05-24 21:34:15 -0300

    star wars cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardsComponent } from './pages/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
